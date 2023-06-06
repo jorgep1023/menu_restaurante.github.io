2f0e1083bd2a3645cce3ce637f20d2932214980d
jorgep1023 <83409896+jorgep1023@users.noreply.github.com>
2023-06-06 16:00:51 -0500

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<head>
+<title></title>
+</head>
+<?php
+$mail = ('jorgep_23_@hotmail.com');
+$nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
+$mensaje = $_POST ['mensaje'];
+
+$message = "
+MENSAJE DE CONTACTO
+
+Nombre:     "  .$nombre."
+Correo:     "  .$email."
+Mensaje:    "  .$mensaje."
+"; 
+  (mail($mail,"NUEVO MENSAJE DE PRUEBA ", $message))
+?>
+  <script>alert("Tu mensaje ha sido enviado");</script>
+ <script type="text/javascript">
+  window.location="index.html";
+  </script> 
+</body>
+ </html>
+
